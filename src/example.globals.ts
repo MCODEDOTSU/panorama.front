7a892a440bb2732f13fdd5f9e943719d364b1861
KOVCHENKO <il-kow@mail.ru>
2019-06-07 16:58:29 +0400

    del(globals.ts)

--- src/example.globals.ts
+++ src/example.globals.ts
@@ -1,4 +1,2 @@
 export const baseUrlAPI = 'https://city-panorama.ru/api/';
 export const baseUrl = 'https://city-panorama.ru/';
-// export const baseUrlAPI = 'http://umgorod.back/api/';
-// export const baseUrl = 'http://umgorod.back/';
--- src/globals.js
+++
@@ -1,5 +0,0 @@
-export const baseUrlAPI = 'https://city-panorama.ru/api/';
-export const baseUrl = 'https://city-panorama.ru/';
-// export const baseUrlAPI = 'http://umgorod.back/api/';
-// export const baseUrl = 'http://umgorod.back/';
-//# sourceMappingURL=globals.js.map
