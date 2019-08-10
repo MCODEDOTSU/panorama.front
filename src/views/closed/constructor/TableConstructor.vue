c28a77d2b909e369f88fccf53faf19302611dab0
mcode-su <info@mcode.su>
2019-08-11 01:22:10 +0400

    feat(validation)

--- src/views/closed/constructor/TableConstructor.vue
+++ src/views/closed/constructor/TableConstructor.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div class="row" v-for="tableField in tableFields">
-            <div class="col-3">
+        <div class="row" v-for="(tableField, key) in tableFields">
+            <div class="col-2">
                 <select class="form-control" v-model="tableField.type">
                     <option value="text_field">Текстовое поле</option>
                     <option value="long_text_field">Длинное текстовое поле</option>
@@ -9,16 +9,18 @@
                 </select>
             </div>
             <div class="col-4">
-                <input type="text" class="form-control" placeholder="Наименование поля" v-model="tableField.title">
+                <input type="text" :name="'наименование под номером ' + key" class="form-control" v-validate="'required'" placeholder="Наименование поля" v-model="tableField.title">
+                <span class="validation-error" style="color: #ff0000; font-size: 10pt">{{ errors.first('наименование под номером ' + key) }}</span>
             </div>
             <div class="col-4">
-                <input v-if="constructorState.isTableExists" type="text" class="form-control"
-                       placeholder="Техническое наименование поля" v-model="tableField.new_tech_title">
-                <input v-else type="text" class="form-control" placeholder="Техническое наименование поля"
-                       v-model="tableField.tech_title">
+                <input type="text" class="form-control" v-validate="'required'" :name="'техническое_наименование_под_номером_' + key" placeholder="техническое наименование поля" v-model="tableField.tech_title">
+                <span class="validation-error" style="color: #ff0000; font-size: 10pt">{{ errors.first('техническое_наименование_под_номером_' + key) }}</span>
             </div>
             <div class="col-1">
                 <input type="checkbox" class="form-control" placeholder="Обязательное" v-model="tableField.required">
+            </div>
+            <div class="col-1">
+                <button type="button" class="btn btn-primary" @click="dropColumn(tableField)">X</button>
             </div>
         </div>
 
@@ -35,19 +37,19 @@
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
-    import axios from "axios";
-    import {baseUrlAPI} from "@/globals";
-    import ErrorNotifier from "@/domain/util/notifications/ErrorNotifier";
-    import TableField from "@/domain/entities/constructor/TableField";
-    import ConstructorState from "@/store/modules/constructor/types";
-    import {State} from "vuex-class";
+    import {Component, Vue} from 'vue-property-decorator';
+    import axios from 'axios';
+    import {baseUrlAPI} from '@/globals';
+    import ErrorNotifier from '@/domain/util/notifications/ErrorNotifier';
+    import TableField from '@/domain/entities/constructor/TableField';
+    import ConstructorState from '@/store/modules/constructor/types';
+    import {State} from 'vuex-class';
 
     @Component
     export default class TableConstructor extends Vue {
 
         private tableFields: TableField[] = [];
-        @State("constructor") private constructorState: ConstructorState;
+        @State('constructor') private constructorState: ConstructorState;
 
         public created() {
             this.checkIfTableExists();
@@ -55,9 +57,9 @@
 
         private addField() {
             this.tableFields.push({
-                type: "text_field",
-                title: "",
-                tech_title: "",
+                type: 'text_field',
+                title: '',
+                tech_title: '',
                 required: false,
             });
         }
@@ -92,6 +94,8 @@
                     table_title: this.$route.params.id,
                     columns: this.tableFields,
                 });
+
+                this.getTableInfo();
             } catch {
                 ErrorNotifier.notify();
             }
@@ -101,14 +105,22 @@
             try {
                 const res = await axios.get(`${baseUrlAPI}constructor/get_table_info/${this.$route.params.id}`);
                 this.tableFields = res.data;
-                for (const tableField of this.tableFields) {
-                    tableField.new_tech_title = tableField.tech_title;
-                }
             } catch {
                 ErrorNotifier.notify();
             }
         }
 
+        private async dropColumn(tableField: TableField) {
+            try {
+                const res = await axios.post(`${baseUrlAPI}constructor/drop_column`, {
+                    column_tech_title: tableField.tech_title,
+                    table_id: this.$route.params.id,
+                });
 
+                this.getTableInfo();
+            } catch {
+                ErrorNotifier.notify();
+            }
+        }
     }
 </script>
