88541538ea825aa622a179ddf616b079bbc740ad
KOVCHENKO <il-kow@mail.ru>
2020-02-05 16:22:20 +0300

    upd(App/Home Vues) fixed using linter

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,27 @@
 </template>
 
 <script lang="ts">
+import {Component, Provide, Vue} from 'vue-property-decorator';
+import {Action, State} from 'vuex-class';
+import UserState from '@/store/modules/user/types';
+import Router from '@/router';
 
-    import {Component, Provide, Vue} from "vue-property-decorator";
-    import {Action, State} from "vuex-class";
-    import UserState from "@/store/modules/user/types";
-    import Router from "@/router";
+@Component({})
+export default class App extends Vue {
 
-    @Component({})
-    export default class App extends Vue {
+    @State('user') public userState!: UserState;
+    @Action public getUser: any;
 
-        @State("user") public userState!: UserState;
-        @Action public getUser: any;
-
-        public created() {
-            if (this.userState.token !== "") {
-                this.getUser();
-            } else {
-                Router.push({name: "home"});
-            }
-
+    public created() {
+        if (this.userState.token !== '') {
+            this.getUser();
+        } else {
+            Router.push({name: 'home'});
         }
 
     }
+
+}
 </script>
 
 <style lang="scss">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,57 +26,56 @@
 </template>
 
 <script lang="ts">
+import {Component, Provide, Vue} from 'vue-property-decorator';
+import {Action, State} from 'vuex-class';
+import axios from 'axios';
+import {baseUrlAPI} from '@/globals';
+import {addAuthorizationHeader} from '@/domain/util/libraries/AxiosConfig';
+import ErrorNotifier from '@/domain/util/notifications/ErrorNotifier';
+import UserState from '@/store/modules/user/types';
 
-    import {Component, Provide, Vue} from "vue-property-decorator";
-    import {Action, State} from "vuex-class";
-    import axios from "axios";
-    import {baseUrlAPI} from "@/globals";
-    import {addAuthorizationHeader} from "@/domain/util/libraries/AxiosConfig";
-    import ErrorNotifier from "@/domain/util/notifications/ErrorNotifier";
-    import UserState from "@/store/modules/user/types";
+@Component({})
+export default class Home extends Vue {
 
-    @Component({})
-    export default class Home extends Vue {
+    @Provide()
+    public date = new Date();
 
-        @Provide()
-        public date = new Date();
+    @Provide()
+    public user = {
+        email: '',
+        password: '',
+    };
 
-        @Provide()
-        public user = {
-            email: "",
-            password: "",
-        };
+    @Provide()
+    public isLoad = false;
 
-        @Provide()
-        public isLoad = false;
+    @State('user') public userState!: UserState;
+    @Action public getUser: any;
 
-        @State("user") public userState!: UserState;
-        @Action public getUser: any;
+    public login() {
 
-        public login() {
+        this.isLoad = true;
 
-            this.isLoad = true;
-
-            axios.post(`${baseUrlAPI}login`, this.user).then((response) => {
-                this.userState.token = "Bearer " + response.data.access_token;
-                this.userState.role = response.data.role;
-                addAuthorizationHeader(this.userState);
-                this.getUser();
-                this.isLoad = false;
-            }).catch((error) => {
-                if (error.response && error.response.status === 401) {
-                    ErrorNotifier.notifyWithCustomMessage('Аутентификация не удалась: неверное имя пользователя или пароль');
-                } else if (error.response && error.response.data.msg) {
-                    ErrorNotifier.notifyWithCustomMessage(error.response.data.msg);
-                } else if (error.message) {
-                    ErrorNotifier.notifyWithCustomMessage(error.message);
-                } else {
-                    ErrorNotifier.notifyWithCustomMessage('Аутентификация не удалась: неизвестная ошибка');
-                }
-                this.user.password = '';
-                this.isLoad = false;
-            });
-        }
-
+        axios.post(`${baseUrlAPI}login`, this.user).then((response) => {
+            this.userState.token = 'Bearer ' + response.data.access_token;
+            this.userState.role = response.data.role;
+            addAuthorizationHeader(this.userState);
+            this.getUser();
+            this.isLoad = false;
+        }).catch((error) => {
+            if (error.response && error.response.status === 401) {
+                ErrorNotifier.notifyWithCustomMessage('Аутентификация не удалась: неверное имя пользователя или пароль');
+            } else if (error.response && error.response.data.msg) {
+                ErrorNotifier.notifyWithCustomMessage(error.response.data.msg);
+            } else if (error.message) {
+                ErrorNotifier.notifyWithCustomMessage(error.message);
+            } else {
+                ErrorNotifier.notifyWithCustomMessage('Аутентификация не удалась: неизвестная ошибка');
+            }
+            this.user.password = '';
+            this.isLoad = false;
+        });
     }
+
+}
 </script>
