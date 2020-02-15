d1cb1178d53b57e5c0a807cd42433cbb4d7a2395
il-kow@mail.ru <il-kow@mail.ru>
2020-02-16 00:46:37 +0400

    upd(limitations for text field value)

--- src/components/constructor/fields/TextField.vue
+++ src/components/constructor/fields/TextField.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <input type="text" :id="field.tech_title" v-if="field.type === 'text_field' && field.required === false"
-               class="form-control" v-model="field.value"
-               :placeholder="field.title">
+        <div v-if="field.type === 'text_field' && field.required === false">
+            <input type="text" :id="field.tech_title" class="form-control" v-model="validateNonRequired"
+                   :placeholder="field.title">
+            <span class="value-length">{{ valueLength }}</span>
+        </div>
         <input type="text" :id="field.tech_title" v-if="field.type === 'text_field' && field.required"
                data-vv-validate-on="change|blur" v-validate="'required'" :name="field.title"
-               class="form-control" v-model="field.value"
-               :placeholder="field.title">
+               class="form-control" v-model="validateRequired" :placeholder="field.title">
         <span class="validation-error">{{ errors.first(field.title) }}</span>
     </div>
 </template>
@@ -18,5 +19,46 @@
     export default class TextField extends Vue {
         @Prop() private field: any;
         @Inject('validator') private $validator: any;
+
+        get validateRequired() {
+            if (this.field.value && this.field.value.length >= this.field.options.max) {
+                return this.field.value.substring(0, this.field.options.max - 1);
+            }
+
+            return this.field.value;
+        }
+
+        set validateRequired(value) {
+            this.field.value = value;
+        }
+
+        get validateNonRequired() {
+            if (this.field.value && this.field.value.length >= this.field.options.max) {
+                return this.field.value.substring(0, this.field.options.max - 1);
+            }
+
+            return this.field.value;
+        }
+
+        set validateNonRequired(value) {
+            this.field.value = value;
+        }
+
+        get valueLength() {
+            if (!this.field.value) {
+                this.field.value = '';
+            }
+            return this.field.options.max - this.field.value.length;
+        }
     }
 </script>
+
+<style>
+    .value-length {
+        font-size: 14px;
+        line-height: 1;
+        display: inline-block;
+        padding: 4px 0;
+        color: red;
+    }
+</style>
